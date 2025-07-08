f018703f7de48fa1ff33dabe3d9f37187d4998d8
Deniz2028 <145937547+Deniz2028@users.noreply.github.com>
2025-07-09 00:51:21 +0300

    Delete src directory

--- src/inference/__init__.py
+++
@@ -1,9 +0,0 @@
-# src/inference/__init__.py
-"""Inference module for trained models."""
-
-from .predictor import EnhancedTransformerPredictor, run_inference_cli
-
-__all__ = [
-    'EnhancedTransformerPredictor',
-    'run_inference_cli'
-]
